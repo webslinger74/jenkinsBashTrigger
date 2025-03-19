13b53bb2015a600f470d278f69605f8115c4501c
steven <webster_steven@yahoo.co.uk>
2025-03-19 14:18:50 +0000

    updated jenkinsfile

--- src/main/ci/trigger-update.sh
+++ src/main/ci/trigger-update.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 rm -f response.txt
-
+echo " THE WORKSPACE IN IS ${WORKSPACE}" 
 DESTINATION=$1
 
-URL=$([ "$DESTINATION" == "prod" ] && echo "https://localhost:8080/prod" || echo "https//localhost:8080/dev" )
+URL=$([ "$DESTINATION" == "prod" ] && echo "simplewebapp-myapp-1:8081/upload" || echo "simplewebapp-myapp-1:8081/upload" )
+echo "${URL}"
 
 makePostRequest() {
+
   RETRIES=1
   echo "Requesting: $1"
   set +e
@@ -14,18 +16,21 @@ makePostRequest() {
 
   for run in $( seq 1 $RETRIES)
   do
-    http_response=$(curl --request POST --header 'Content-type: multipart/form-data' -F "csvFile=@${WORKSPACE}/claimants.csv" --output response.txt --write-out "%{http_code" --max-time 7200 "$1" )
+    cat "${WORKSPACE}/claimants.csv"
+    response1=$(curl simplewebapp-myapp-1:8081/upload --max-time 17200)
+    echo "${response1}"
+    http_response=$(curl --request POST --header 'Content-type: multipart/form-data' -F "csvFile=@${WORKSPACE}/claimants.csv" --output response.txt --write-out "%{http_code}" --max-time 17200 "$1" )
   if [ "$http_response" == "200" ]; then
     echo "SUCCESS Response Code: $http_response, response: $(cat response.txt), at: $(date)"
     return
   fi
-  echo "Status code was: $http_response,  retry: ${run}/${RETRIES}"
+  echo "Status code was: ${http_response},  retry: ${run}/${RETRIES}"
   sleep 5
   done
   set -e
 
-  echo "FAILED, response code: $http_response, response: $(cat response.txt), after ${RETRIES} retries, time now: $(data)"
+  echo "FAILED, response code: $http_response, response: $(cat response.txt), after ${RETRIES} retries, time now: $(date)"
   exit 1;
 }
 
-makePostRequest "$URL/trigger-update"
+makePostRequest "$URL"
